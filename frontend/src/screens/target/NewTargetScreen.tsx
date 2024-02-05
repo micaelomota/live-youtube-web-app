08023dd72a0ff0f33c13794c6645c09a4cef32b5
Iarlen <iarlensr@hotmail.com>
2024-02-05 04:08:23 -0300

    feat: adding validation of inputs to create new target

--- frontend/src/screens/target/NewTargetScreen.tsx
+++ frontend/src/screens/target/NewTargetScreen.tsx
@@ -1,53 +1,75 @@
-import { Button, Form, Input } from "antd";
-import { useTargets } from "../../context/TargetContext";
+import { Form } from "antd";
 import { useNavigate } from "react-router-dom";
+import InputForm from "../../components/InputForm";
+import SelectForm from "../../components/SelectForm";
+import ButtonForm from "../../components/ButtonForm";
+import { useTargets } from "../../context/TargetContext";
+
+interface Target {
+  name: string;
+  unit: string;
+  currentValue: number;
+  createdAt: string;
+  updatedAt: string;
+}
 
 export const NewTargetScreen: React.FC = () => {
-  const { addTarget } = useTargets();
+  const [form] = Form.useForm<Target>();
   const navigate = useNavigate();
+  const { addTarget } = useTargets();
 
-  const onFinishForm = (values: any) => {
-    console.log(values);
+  const onFinishForm = (values: Target) => {
     addTarget({
       ...values,
       currentValue: Number(values.currentValue),
-      // TODO: remove isso aqui pq eu já tenho no backend
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
     });
     navigate("/");
   };
 
-  // TODO: validar o formulário antes de salvar
-
   return (
     <div>
-      <Form onFinish={onFinishForm}>
-        <Form.Item label="Nome da meta" name="name">
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Quantidade" name="target">
-          <Input type="number" />
-        </Form.Item>
-
-        <Form.Item label="Unidade" name="unity">
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Prazo" name="deadline">
-          <Input type="date" />
-        </Form.Item>
-
-        <Form.Item label="Valor inicial" name="currentValue">
-          <Input type="number" />
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Salvar
-          </Button>
-        </Form.Item>
+      <Form onFinish={onFinishForm} layout="vertical" form={form}>
+        <InputForm
+          label="Nome da meta"
+          name="name"
+          placeholder="Nome da meta"
+          rules={[{ required: true, message: "Campo obrigatório." }]}
+        />
+        <InputForm
+          label="Quantidade"
+          name="target"
+          placeholder="Quantidade"
+          type="number"
+          rules={[{ required: true, message: "Campo obrigatório." }]}
+        />
+        <SelectForm
+          label="Unidade"
+          name="unity"
+          placeholder="Selecione a unidade"
+          rules={[{ required: true, message: "Campo obrigatório." }]}
+          options={[
+            { value: "km", label: "Km (Quilometro)" },
+            { value: "kg", label: "Kg (Kilograma)" },
+            { value: "horas", label: "Hrs (Horas)" },
+            { value: "minutos", label: "Min (Minutos)" },
+            { value: "vezes", label: "Vzs (Vezes)" },
+          ]}
+        />
+        <InputForm
+          label="Prazo"
+          name="deadline"
+          placeholder="Selecione o prazo"
+          type="date"
+          rules={[{ required: true, message: "Campo obrigatório." }]}
+        />
+        <InputForm
+          label="Valor inicial"
+          name="currentValue"
+          placeholder="Valor inicial"
+          type="number"
+          rules={[{ required: true, message: "Campo obrigatório." }]}
+        />
+        <ButtonForm text="Salvar" type="primary" htmlType="submit" />
       </Form>
     </div>
   );
