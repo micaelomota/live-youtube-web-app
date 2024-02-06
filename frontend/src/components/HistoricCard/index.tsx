6fc3034f85fc7d6cae19cfa73b63c1456db1b55e
Iarlen <iarlensr@hotmail.com>
2024-02-05 21:46:25 -0300

    feat: creating TargetDetail page and HistoricCard component.

---
+++ frontend/src/components/HistoricCard/index.tsx
@@ -0,0 +1,28 @@
+import { Flex, List } from "antd";
+import React from "react";
+import { convertDate } from "../../utils/convertDate";
+
+interface HistoricCardProps {
+  date: string;
+  notes?: string;
+  unit: string;
+  value: number;
+}
+const HistoricCard = ({ date, value, unit, notes }: HistoricCardProps) => {
+  const dateFormated = convertDate(date.toString());
+  return (
+    <List.Item>
+      <Flex vertical gap={0}>
+        <p className="text-xl">{dateFormated}</p>
+        <div className="flex flex-col gap-1">
+          <span className="text-lg font-semibold text-blue-600">
+            {value} {unit}
+          </span>
+          {notes && <span className="text-sm text-green-600">{notes}</span>}
+        </div>
+      </Flex>
+    </List.Item>
+  );
+};
+
+export default HistoricCard;
