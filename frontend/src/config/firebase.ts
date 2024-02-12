81b1eccf17e370f064589438588daaa0d8b31310
Micael Mota <micaelomota@gmail.com>
2024-02-11 19:40:07 -0500

    setup firebase

---
+++ frontend/src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAJ8g-StTPg7YsSEOjYHBFNfG95J7wVqY",
+  authDomain: "live-micael-01.firebaseapp.com",
+  projectId: "live-micael-01",
+  storageBucket: "live-micael-01.appspot.com",
+  messagingSenderId: "238858151488",
+  appId: "1:238858151488:web:7983bd229797773f1521a5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
