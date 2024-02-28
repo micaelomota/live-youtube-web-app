47e5b206d622ec912b309d23aa41b500b89620c1
Jonas Dias <zdias1413@gmail.com>
2024-02-28 00:10:37 -0300

    feat: improve the sign in page

---
+++ frontend/src/screens/signin/SignIn.styles.ts
@@ -0,0 +1,53 @@
+import React from "react";
+
+const layout: React.CSSProperties = {
+  height: '100vh',
+  maxWidth: '1580px',
+  margin: '0 auto',
+  fontFamily: 'monospace, sans-serif',
+};
+
+const content: React.CSSProperties = {
+  backgroundColor: '#FFF',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+  
+const sider: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#FFF',
+  padding: '50px 40px',
+};
+
+const siderContent: React.CSSProperties = {
+  textAlign: 'right',
+  marginTop: '40px',
+}
+
+const button: React.CSSProperties = {
+  height: '40px',
+  display: 'flex',
+  alignItems: 'center',
+}
+
+const image: React.CSSProperties = {
+  width: '280px',
+}
+
+const card: React.CSSProperties = {
+  maxWidth: '380px',
+  width: '90%',
+}
+
+export const signInStyles = {
+  layout,
+  content,
+  sider,
+  button,
+  image,
+  siderContent,
+  card
+}
--- frontend/src/screens/signin/SignInScreen.tsx
+++ frontend/src/screens/signin/SignInScreen.tsx
@@ -1,6 +1,11 @@
-import { Button } from "antd";
+import { useState } from "react";
+import { Button, Card, Divider, Layout } from "antd";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../config/firebase";
+import { signInStyles } from "./SignIn.styles";
+import googleLogo from "../../assets/googleLogo.svg";
+import logo from "../../assets/logo.svg";
+import { SignInForm } from "../../components/SignInForm";
 
 const onClickSignInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
@@ -14,14 +19,41 @@ const onClickSignInWithGoogle = async () => {
 };
 
 export const SignInScreen = () => {
+  const [isBroken, setIsBroken] = useState(false);
+  
   return (
-    <div>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
-      {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} /> */}
-      <Button size="large" onClick={onClickSignInWithGoogle}>
-        Sign in with Google
-      </Button>
-    </div>
+    <Layout style={signInStyles.layout}>
+      <Layout.Content style={signInStyles.content}>
+        <Card style={signInStyles.card}>
+          <h1>Faça Login</h1>
+          <SignInForm />
+          <Divider />
+          <Button 
+            style={signInStyles.button}
+            onClick={onClickSignInWithGoogle}
+            icon={<img src={googleLogo} alt="Logo da Google." width="15px" />}
+          >
+            Google
+          </Button>
+        </Card>
+      </Layout.Content>
+      <Layout.Sider 
+        style={signInStyles.sider} 
+        width={500}
+        hidden={isBroken}
+        breakpoint="lg"
+        onBreakpoint={(broken) => setIsBroken(broken)}
+      >
+        <img src={logo} alt="Logo do Progress Tracking" style={signInStyles.image}/>
+        <Layout.Content style={signInStyles.siderContent}>
+          <h1>
+              Bem-vindo ao Progress Tracking
+            </h1>
+            <p>
+              Faça login e defina suas metas
+            </p>
+        </Layout.Content>
+      </Layout.Sider>
+    </Layout>
   );
 };
