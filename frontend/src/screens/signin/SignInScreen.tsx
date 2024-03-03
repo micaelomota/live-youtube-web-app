efe1e6ff73078d90a7a1b6c885646624f166911e
Silas <dev.silas.freitas@outlook.com>
2024-03-03 15:41:50 -0300

    Add the button signin and signout

--- frontend/src/screens/signin/SignInScreen.tsx
+++ frontend/src/screens/signin/SignInScreen.tsx
@@ -1,25 +1,29 @@
 import { Button } from "antd";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 const onClickSignInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
   await signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    })
+  .then((result) => {
+    console.log(result);
+  })
     .catch((error) => {
       console.log(error);
     });
 };
 
 export const SignInScreen = () => {
+  const navigate = useNavigate()
   return (
     <div>
       <h1>My App</h1>
       <p>Please sign-in:</p>
       {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} /> */}
-      <Button size="large" onClick={onClickSignInWithGoogle}>
+      <Button size="large" onClick={() => {
+        onClickSignInWithGoogle().then(() => navigate("/"))
+      }}>
         Sign in with Google
       </Button>
     </div>
