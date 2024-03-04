0d2d2f4905403240c261e7f9c2865c133cabfad0
André Bordignon <andrebordignonn@gmail.com>
2024-03-03 21:09:45 -0300

    Add functions to redirect the user on sign-in or sign-out (#18)
    * chore: add redirect on login and logout actions
    
    * fix: add User type
    
    * fix: remove npm files (using yarn instead)
    
    * remove unused imports

--- frontend/src/screens/signin/SignInScreen.tsx
+++ frontend/src/screens/signin/SignInScreen.tsx
@@ -1,25 +1,39 @@
 import { Button } from "antd";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../config/firebase";
-
-const onClickSignInWithGoogle = async () => {
-  const provider = new GoogleAuthProvider();
-  await signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+import { useNavigate } from "react-router-dom";
 
 export const SignInScreen = () => {
+  const navigate = useNavigate();
+
+  const onClickSignInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+
+    const result = await signInWithPopup(auth, provider)
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return result;
+  };
+
+  const handleSignIn = async () => {
+    await onClickSignInWithGoogle()
+      .then(() => navigate("/"))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>My App</h1>
       <p>Please sign-in:</p>
       {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} /> */}
-      <Button size="large" onClick={onClickSignInWithGoogle}>
+      <Button size="large" onClick={handleSignIn}>
         Sign in with Google
       </Button>
     </div>
